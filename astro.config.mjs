28c38243e1253e55c381dd3888e0eaa0e60e219e
shidari <s.hidari@icloud.com>
2025-08-25 00:32:17 +0900

    fix: siteの修正 - これでmdも表示されるか?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://shidari.github.io',
+  site: 'https://shidari.github.io/my-portfolio/',
   base: '/my-portfolio/',
 })
